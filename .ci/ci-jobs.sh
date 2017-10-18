4c60b35c3dddc3ec55909ebed9fc1280627ea5c7
Archana Shinde <archana.m.shinde@intel.com>
2017-10-18 10:54:05 -0700

    ci: dont pass env
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

--- .ci/ci-jobs.sh
+++ .ci/ci-jobs.sh
@@ -22,8 +22,8 @@ export CI=true
 
 make check
 
-sudo -E go test -bench=.
+sudo  go test -bench=.
 
-sudo -E go test -bench=CreateStartStopDeletePodQemuHypervisorNoopAgentNetworkCNI -benchtime=60s
+sudo  go test -bench=CreateStartStopDeletePodQemuHypervisorNoopAgentNetworkCNI -benchtime=60s
 
-sudo -E go test -bench=CreateStartStopDeletePodQemuHypervisorHyperstartAgentNetworkCNI -benchtime=60s
+sudo  go test -bench=CreateStartStopDeletePodQemuHypervisorHyperstartAgentNetworkCNI -benchtime=60s
